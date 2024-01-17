06b1b65f06c186bec89f864a96422175cbab0152
p4vel <versiontv1@gmail.com>
2024-01-17 16:13:31 +0200

    added 2 screens to userStack

--- navigation/userStack.js
+++ navigation/userStack.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../screens/HomeScreen';
+import ExpensesScreen from '../screens/ExpensesScreen';
+import IncomesScreen from '../screens/IncomesScreen';
 const Stack = createStackNavigator();
 
 export default function UserStack() {
@@ -10,6 +12,8 @@ export default function UserStack() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Expenses" component={ExpensesScreen} />
+        <Stack.Screen name="Incomes" component={IncomesScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
