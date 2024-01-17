b029dac25bad40fbd47d855be5f98417ae91b368
p4vel <versiontv1@gmail.com>
2024-01-17 16:12:34 +0200

    IncomesScreen with Incomes list and add button

---
+++ screens/IncomesScreen.js
@@ -0,0 +1,199 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity ,Modal} from 'react-native';
+import { useAuthentication } from '../utils/hooks/useAuthentication';
+import { getFirestore, collection, getDocs, deleteDoc, doc,addDoc } from 'firebase/firestore';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button, Input} from 'react-native-elements';
+import { Picker } from '@react-native-picker/picker';
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+const db = getFirestore();
+
+export default function IncomesScreen({ navigation }) {
+  const { user } = useAuthentication();
+  const [incomes, setIncomes] = useState([]);
+  const [newIncome, setNewIncome] = useState('');
+  const [incomeAmount, setIncomeAmount] = useState('');
+  const [incomeDatetime, setIncomeDatetime] = useState(new Date().toISOString());
+  const [selectedIncomeType, setSelectedIncomeType] = useState('salary');
+  const [incomeComment, setIncomeComment] = useState('');
+  const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (user) {
+        await fetchIncomes();
+      }
+    };
+
+    fetchData();
+  }, [user]);
+
+  const toggleIncomeModal = () => {
+    setIsIncomeModalVisible(!isIncomeModalVisible);
+  };
+
+
+  const fetchIncomes = async () => {
+    try {
+      const incomesCollection = collection(db, 'users', user?.uid, 'incomes');
+      const incomesSnapshot = await getDocs(incomesCollection);
+      const incomesData = incomesSnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      setIncomes(incomesData);
+    } catch (error) {
+      console.error('Error fetching incomes:', error);
+    }
+  };
+
+  const addIncome = async () => {
+    try {
+      const incomesCollection = collection(db, 'users', user?.uid, 'incomes');
+      await addDoc(incomesCollection, {
+        description: newIncome,
+        amount: parseFloat(incomeAmount) || 0,
+        datetime: incomeDatetime,
+        type: selectedIncomeType,
+        comment: incomeComment,
+      });
+      setNewIncome('');
+      setIncomeAmount('');
+      setIncomeDatetime(new Date().toISOString());
+      setSelectedIncomeType('salary');
+      setIncomeComment('');
+      fetchIncomes();
+      toggleIncomeModal();
+    } catch (error) {
+      console.error('Error adding income:', error);
+    }
+  };
+
+  const deleteIncome = async (incomeId) => {
+    try {
+      const incomeDoc = doc(db, 'users', user?.uid, 'incomes', incomeId);
+      await deleteDoc(incomeDoc);
+      fetchIncomes();
+    } catch (error) {
+      console.error('Error deleting income:', error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Incomes</Text>
+      <Button style={{ marginBottom: 25 }} title="Enter Income" onPress={toggleIncomeModal} />
+      {/* Income Modal */}
+      <Modal visible={isIncomeModalVisible} animationType="slide" transparent={true}>
+        <View style={styles.modalContainer}>
+          <View style={[styles.modalContent, { width: '80%' }]}>
+            <Text style={styles.modalTitle}>Enter Income</Text>
+            <Input
+              placeholder="Income Description"
+              value={newIncome}
+              onChangeText={(text) => setNewIncome(text)}
+              containerStyle={styles.inputContainer}
+            />
+            <Input
+              placeholder="Income Amount"
+              value={incomeAmount}
+              onChangeText={(text) => setIncomeAmount(text)}
+              keyboardType="numeric"
+              containerStyle={styles.inputContainer}
+            />
+            <DateTimePicker
+              style={styles.inputContainer}
+              value={new Date(incomeDatetime)}
+              mode="datetime"
+              display="default"
+              onChange={(event, date) => setIncomeDatetime(date.toISOString())}
+            />
+            <Picker
+              selectedValue={selectedIncomeType}
+              onValueChange={(itemValue) => setSelectedIncomeType(itemValue)}
+            >
+              <Picker.Item label="Salary" value="salary" />
+              <Picker.Item label="Side Job" value="sidejob" />
+              <Picker.Item label="Transaction" value="transaction" />
+              <Picker.Item label="Gift" value="gift" />
+              <Picker.Item label="Other" value="other" />
+            </Picker>
+            <Input
+              placeholder="Comment"
+              value={incomeComment}
+              onChangeText={(text) => setIncomeComment(text)}
+              containerStyle={styles.inputContainer}
+            />
+
+            <Button title="Add Income" onPress={addIncome} />
+            <Button title="Cancel" type="outline" onPress={toggleIncomeModal} />
+          </View>
+        </View>
+      </Modal>
+      <FlatList
+        data={incomes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.incomeItem}
+            onPress={() => navigation.navigate('IncomeDetails', { income: item })}
+          >
+            <Text style={styles.incomeDescription}>{item.description}</Text>
+            <Text style={styles.incomeAmount}>{item.amount}</Text>
+            <TouchableOpacity
+              onPress={() => deleteIncome(item.id)}
+              style={styles.deleteButton}
+            >
+              <Icon name="times" size={15} color="red" />
+            </TouchableOpacity>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  incomeItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+    paddingVertical: 10,
+  },
+  incomeDescription: {
+    flex: 2,
+  },
+  incomeAmount: {
+    flex: 1,
+  },
+  deleteButton: {
+    padding: 5,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+});
