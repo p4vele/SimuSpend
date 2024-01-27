3b767dea0a8ec43bec993db98616ef905d0d4153
p4vel <versiontv1@gmail.com>
2024-01-27 18:16:24 +0200

    dependencies and expo upgrade and adjustment

--- app.config.js
+++ app.config.js
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 
-
-export default{
+export default {
   "expo": {
     "name": "SimuSpend",
     "slug": "SimuSpend",
@@ -9,6 +8,7 @@ export default{
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
+    "platforms": ["ios", "android"],
     "splash": {
       "image": "./assets/splash.png",
       "resizeMode": "contain",
@@ -17,28 +17,47 @@ export default{
     "updates": {
       "fallbackToCacheTimeout": 0
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
+    "assetBundlePatterns": ["**/*"],
     "ios": {
-      "supportsTablet": true
+      "supportsTablet": true,
+      "infoPlist": {
+        "NSCameraUsageDescription": "Allow access to your photo library",
+        "NSPhotoLibraryUsageDescription": "Allow access to your photo library"
+      }
     },
+    "plugins": [
+      [
+        "expo-document-picker",
+        {
+          "iCloudContainerEnvironment": "Production"
+        }
+      ]
+    ],
     "android": {
+      "package": "com.SimuSpend",
+      "versionCode": 1,
+      "permissions": ["CAMERA_ROLL","READ_EXTERNAL_STORAGE", "WRITE_EXTERNAL_STORAGE"],
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
+      },
+      "config": {
+        "googleSignIn": {
+          "apiKey": "your-google-signin-api-key",
+          "certificateHash": "your-certificate-hash"
+        }
       }
     },
     "web": {
       "favicon": "./assets/favicon.png"
     },
     "extra": {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID
-    },
+      "firebaseApiKey": process.env.FIREBASE_API_KEY,
+      "firebaseAuthDomain": process.env.FIREBASE_AUTH_DOMAIN,
+      "firebaseProjectId": process.env.FIREBASE_PROJECT_ID,
+      "firebaseStorageBucket": process.env.FIREBASE_STORAGE_BUCKET,
+      "firebaseMessagingSenderId": process.env.FIREBASE_MESSAGING_SENDER_ID,
+      "firebaseAppId": process.env.FIREBASE_APP_ID
+    }
   }
-}
+};
